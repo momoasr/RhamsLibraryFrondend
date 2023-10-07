d3b5f61d7797a15e3a35f6200144dd141ed57b2f
momoasr <monia_akele@yahoo.fr>
2023-10-06 21:28:07 -0400

    add search-book component html

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  searchBookFormGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private http:HttpClient){ }
+
+  ngOnInit(){
+    this.searchBookFormGroup = this.fb.group({
+      
+    })
+  }
+  
 
 }
